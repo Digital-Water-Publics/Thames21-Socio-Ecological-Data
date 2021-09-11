006603bc4f45cbddfaf00541cc51a99b3db684d7
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-11 15:19:15 +0100

    init commit // load libs

---
+++ pot-mi/scripts/libs.R
@@ -0,0 +1,12 @@
+pkgs = c("academictwitteR",
+         "dplyr",
+         "sf")
+
+# Install pkgs not yet installed
+installed_packages = pkgs %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(pkgs[!installed_packages])
+}
+
+# Load pkgs
+invisible(lapply(pkgs, library, character.only = TRUE))
