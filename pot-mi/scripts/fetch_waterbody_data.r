cfb6d4dd9775eefc0983fc9864254475e1a4cae1
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-18 01:34:46 +0100

    working example for water body mention mine

--- pot-mi/scripts/fetch_waterbody_data.r
+++ pot-mi/scripts/fetch_waterbody_data.r
@@ -1,3 +1,7 @@
 ##### DOWNLOAD WATERBODY DATA HOSTED ON LONDON DATASTORE
 
 thames = search_names(string="London", column="MC")
+
+lark_hawstead<-get_status(ea_name="GB106039023260", column="WBID")
+
+lark_OC_rivers<-get_status(ea_name="London", column="MC", startyr=2019, endyr=2019, type="River")
--- pot-mi/scripts/mine_blue.R
+++ pot-mi/scripts/mine_blue.R
@@ -1,25 +1,36 @@
 # Twitter Mine ------------------------------------------------------------
-tweets = get_all_tweets(
-  query = c(
-    "EnvAgency",
-    "EnvAgencyYNE",
-    "EnvAgencyNW",
-    "EnvAgencySE",
-    "EnvAgencySW",
-    "envagencymids",
-    "EnvAgencyAnglia"
-  ),
-  is_retweet = FALSE,
-  lang = "en",
-  start_tweets = "2017-06-01T00:00:00Z", #TODO discuss with Helge - should this go back to 2016?
-  end_tweets = "2021-08-01T00:00:00Z",
-  bearer_token = get_bearer(),
-  data_path = "data/",
-  bind_tweets = TRUE,
-  context_annotations = FALSE,
-  page_n = 500,
-  n = Inf
-)
+mine_blue_mentions = function(river_query) {
+  river_query = as.character(river_query)
 
-# Clean and write csv --------------------------------------------------------------
-dd_tt = bind_tweets(data_path = "data6/", output_format = "tidy")
+  tweets = get_all_tweets(
+    query = river_query,
+    is_retweet = FALSE,
+    lang = "en",
+    start_tweets = "2016-09-01T00:00:00Z", #TODO discuss with Helge - should this go back to 2016?
+    end_tweets = "2021-09-01T00:00:00Z",
+    bearer_token = get_bearer(),
+    data_path = "data/mined_tweets",
+    bind_tweets = TRUE,
+    context_annotations = FALSE,
+    page_n = 500,
+    n = Inf
+  )
+
+  # Clean and write csv --------------------------------------------------------------
+  river_tweets = bind_tweets(data_path = "data/mined_tweets", output_format = "tidy")
+
+  #set query
+  river_tweets$query = river_query
+
+  #delete files
+  do.call(file.remove, list(list.files("data/mined_tweets", full.names = TRUE)))
+
+  file_path = paste0("data/river_queries/",river_query,".rds")
+
+  #save river file
+  saveRDS(river_tweets, file_path)
+}
+
+mine_blue_mentions(river_query = "gores brook")
+
+
