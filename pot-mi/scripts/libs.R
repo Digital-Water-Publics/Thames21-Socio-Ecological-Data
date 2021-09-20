0168b395a33acbf83aae3220b5caa285229f16be
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-21 00:21:38 +0100

    final methods for cleaning slack data

--- pot-mi/scripts/libs.R
+++ pot-mi/scripts/libs.R
@@ -21,7 +21,12 @@ aa = as.data.frame(str_split_fixed(tt$Name.Query., ";", 2)) # split into two col
 aa$V2 = gsub('.{1}$', '', aa$V2) # remove trailing ';
 
 aa$V1 = gsub('[[:digit:]]+', '', aa$V1) # remove digits
-aa$V1 = aa$V1 %>%str_remove_all("[[:punct:]]") %>% str_trim("both") # remove punct and trim
+aa$V1 = aa$V1 %>% str_remove_all("[[:punct:]]") %>% str_trim("both") # remove punct and trim
 
+aa = aa %>%
+  rename(name = V1) %>%
+  rename(mine_query = V2)
 
+aa = right_join(aa, thames)
 
+write.csv()
