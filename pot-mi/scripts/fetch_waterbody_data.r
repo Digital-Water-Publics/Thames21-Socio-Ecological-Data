a752a178530611bbaa8407827963d5920e8a6e65
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-21 00:11:59 +0100

    get thames data

--- pot-mi/scripts/fetch_waterbody_data.r
+++ pot-mi/scripts/fetch_waterbody_data.r
@@ -2,15 +2,10 @@
 
 #OPTIONS
 
-#1 using london management catchment
-thames = search_names(string="London", column="MC")
+#get thames river catchment data
+thames = search_names(string="Thames", column="RBD")
 
 # use old sf file
 mapbox = read_sf("mapbox/mapbox_primary.shp")
 
 mapbox_min = mapbox %>% distinct(watrbdy,WtrbdID)
-
-
-lark_hawstead<-get_status(ea_name="GB106039023260", column="WBID")
-
-lark_OC_rivers<-get_status(ea_name="London", column="MC", startyr=2019, endyr=2019, type="River")
