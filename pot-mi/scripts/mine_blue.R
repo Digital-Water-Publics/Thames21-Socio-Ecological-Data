4b4a256108668682c6677fd82a65492eaa43a43a
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-23 17:16:20 +0100

    run mine query

--- pot-mi/scripts/mine_blue.R
+++ pot-mi/scripts/mine_blue.R
@@ -1,43 +1,54 @@
 # AIM: Individual dataframes for mentions on twitter for each Thames catchment water body
 
 # Twitter Mine ------------------------------------------------------------
-mine_blue_mentions = function(river_query,wbid) {
-  river_query = as.character(river_query)
+mine_blue_mentions = function(river_query, wbid) {
+  tryCatch(
+    expr = {
+      river_query = as.character(river_query)
 
-    tweets = get_all_tweets(
-      query = river_query,
-      is_retweet = FALSE,
-      lang = "en",
-      start_tweets = "2006-04-01T00:00:00Z", #TODO discuss with Helge - should this go back to 2016?
-      end_tweets = "2021-09-01T00:00:00Z",
-      bearer_token = get_bearer(),
-      data_path = "data/mined_tweets",
-      bind_tweets = TRUE,
-      context_annotations = FALSE,
-      page_n = 500,
-      n = Inf
-    )
+      tweets = get_all_tweets(
+        query = river_query,
+        is_retweet = FALSE,
+        lang = "en",
+        start_tweets = "2006-04-01T00:00:00Z",
+        #TODO discuss with Helge - should this go back to 2016?
+        end_tweets = "2021-09-01T00:00:00Z",
+        bearer_token = get_bearer(),
+        data_path = "data/mined_tweets",
+        bind_tweets = TRUE,
+        context_annotations = FALSE,
+        page_n = 500,
+        n = Inf
+      )
 
+      # Clean and write csv --------------------------------------------------------------
+      river_tweets = bind_tweets(data_path = "data/mined_tweets", output_format = "tidy") %>%
+        mutate(query = river_query) %>%
+        mutate(WBID = wbid)
 
-  # Clean and write csv --------------------------------------------------------------
-  river_tweets = bind_tweets(data_path = "data/mined_tweets", output_format = "tidy") %>%
-    mutate(query = river_query) %>%
-    mutate(WBID = wbid)
+      #delete files
+      do.call(file.remove, list(list.files("data/mined_tweets", full.names = TRUE)))
 
-  #delete files
-  do.call(file.remove, list(list.files("data/mined_tweets", full.names = TRUE)))
+      file_path = paste0("data/river_queries/", river_query, ".rds")
 
-  file_path = paste0("data/river_queries/",river_query,".rds")
-
-  #save river file
-  saveRDS(river_tweets, file_path)
+      #save river file
+      saveRDS(river_tweets, file_path)
+      message(paste("Successfully mined mentions of ", river_query, sep = ""))
+    },
+    error = function(e) {
+      message(
+        'An error has occured. The machine will delete all mined tweets for river query and rerun the river query'
+      )
+      do.call(file.remove, list(list.files("data/mined_tweets", full.names = TRUE)))
+    },
+    finally = {
+      message('All done, quitting.')
+    }
+  )
 }
 
-for(i in 1:nrow(waterbodies)){
-  log_calculator(river_query = waterbodies$mine_query[i],wbid = waterbodies$WBID[i])
+# run mine
+for (i in 1:nrow(water_bodies)) {
+  mine_blue_mentions(river_query = water_bodies$mine_query[i], wbid = water_bodies$WBID[i])
 }
-
-
-
-
 
