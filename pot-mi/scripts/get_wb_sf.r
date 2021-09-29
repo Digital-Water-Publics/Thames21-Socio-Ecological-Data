25fa69ad20d99732b6c580ca2de444f7a9a4e793
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-09-29 02:11:36 +0100

    datum

---
+++ pot-mi/scripts/get_wb_sf.r
@@ -0,0 +1,65 @@
+####
+####
+#### AIM: fetch waterbody geojson for different waterbody classication groups and transform them into sf objects
+####
+####
+
+#### GET WBID DATA FROM CDE PACKAGE
+url = "https://raw.githubusercontent.com/ropensci/cde/master/data-raw/ea_wbids.csv"
+download.file(url, "data/ea_wbids.csv")
+
+#### READ DATA
+wbids = read.csv("data/ea_wbids.csv")
+
+#### FUNCTION TO GET SF FEATURES OF WATERBODY
+get_wb_sf = function(string, #### STRING = NAME OF CLASSFICATION AREA
+                     column) #### COLUMN  = CLASSIFICATION TYPE
+  {
+  #### LOGICAL OPERATOR FOR RIVER MINE
+  if (column == "OC") {wb = wbids %>% filter(OC == string)} # OPERATIONAL CATCHMENT
+  if (column == "MC") {wb = wbids %>% filter(MC == string)} # MANAGMENT CATCHMENT
+  if (column == "RBD") {wb = wbids %>% filter(RBD == string)} # RIVER BASIN DISTRICT
+
+  #### SET EMPTY DF TO MERGE INTO
+  nrows = 1
+  wb_sf = st_sf(id = 1:nrows, geometry = st_sfc(lapply(1:nrows, function(x)
+    st_geometrycollection())))
+  st_crs(wb_sf) = 4326
+  wb_sf$name = ""
+  wb_sf$id = as.character(wb_sf$id)
+  wb_sf = wb_sf %>% filter(name == "nun")
+
+
+  #### LOOP THROUGH GEOJSON DOWNLOAD
+  for (i in 1:nrow(wb)) {
+    ##### EA CATCHMNET API CALL
+    url = "https://environment.data.gov.uk/catchment-planning/WaterBody/"
+    notation = wb$WBID[i]
+    download_url = paste0(url, notation, ".geojson")
+
+    #### SET OUTPUT PATH
+    river_wbid = wb$WBID[i]
+    path = "data/river_sf/"
+    river_output = paste0(path, river_wbid, ".geojson")
+
+    #### DOWNLOOAD FILE, AT LEAST TRY TO
+    tryCatch(
+      expr = {
+        download.file(download_url, river_output)
+      },
+      error = function(e) {
+        message("Unable to download River Please check column and string are correct")
+      }
+    )
+
+    river_sf = read_sf(river_output) %>% select(id, name)
+
+    wb$geometry[1] = river_sf$geometry
+
+    #### REMOPVE DOWNLOADED FILE
+    file.remove(river_output)
+  }
+  return(wb)
+}
+
+pet_sf = get_wb_sf(string = "Waver-Wampool", column = "OC")
