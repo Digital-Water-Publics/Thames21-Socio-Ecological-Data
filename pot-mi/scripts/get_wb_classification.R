7c21bd134915445eae711ee25e776cd07be7fb72
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-10-25 13:19:18 +0100

    update classification script so it works

--- pot-mi/scripts/get_wb_classification.R
+++ pot-mi/scripts/get_wb_classification.R
@@ -4,9 +4,11 @@
 ####
 ####
 
+#### READ DATA
+wbids = read.csv("data/ea_wbids.csv")
 
 #### FUNCTION TO GET SF FEATURES OF WATERBODY
-get_wb_sf = function(string, #### STRING = NAME OF CLASSFICATION AREA
+get_wb_classification = function(string, #### STRING = NAME OF CLASSFICATION AREA
                      column)
   #### COLUMN  = CLASSIFICATION TYPE E.G. OC | MC | RBD
 {
@@ -21,21 +23,12 @@ get_wb_sf = function(string, #### STRING = NAME OF CLASSFICATION AREA
     wb = wbids %>% filter(RBD == string)
   } # RIVER BASIN DISTRICT
 
-  #### SET EMPTY DF TO MERGE INTO
-  nrows = 1
-  wb_sf = st_sf(id = 1:nrows, geometry = st_sfc(lapply(1:nrows, function(x)
-    st_geometrycollection())))
-  st_crs(wb_sf) = 4326 #### SET CRS TO MATCH THAT OF THE EA
-  wb_sf$name = ""
-  wb_sf$id = as.character(wb_sf$id)
-  wb_sf = wb_sf %>% filter(name == "nun") #### CLEAR ANY VALUES IN DF
-
   #### LOOP THROUGH GEOJSON DOWNLOAD
   suppressWarnings(for (i in 1:nrow(wb)) {
     ##### EA CATCHMNET API CALL
     url = "https://environment.data.gov.uk/catchment-planning/WaterBody/"
     notation = wb$WBID[i]
-    download_url = paste0(url, notation, "/classification.csv")
+    download_url = paste0(url, notation, "/classifications.csv")
 
     #### SET OUTPUT PATH
     river_wbid = wb$WBID[i]
@@ -52,12 +45,16 @@ get_wb_sf = function(string, #### STRING = NAME OF CLASSFICATION AREA
       }
     )
 
-    river_sf = read.csv(river_output) %>%  filter(Classification.Item == "Ecological")
+    river_sf = read.csv(river_output) %>%  filter(Classification.Item == "Ecological") %>% filter(Year == 2019)
 
-    wb$geometry[i] = river_sf$geometry[1]
+    wb$status[i] = river_sf$Status[1]
+    wb$year[i] = river_sf$Year[1]
 
     #### REMOPVE DOWNLOADED FILE
     file.remove(river_output)
   })
   return(wb)
 }
+
+# test function
+wb_class = get_wb_classification(string = "Thames",column = "RBD")
