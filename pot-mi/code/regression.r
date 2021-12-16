379e08c829ba0c7a2c0c74d89ef6bb56a0ed9f48
Nathanael Sheehan <nathanaelsheehan@gmail.com>
2021-12-16 00:53:24 +0000

    reg your engienes

--- pot-mi/code/regression.r
+++ pot-mi/code/regression.r
@@ -3,13 +3,15 @@ library(tidyverse)
 
 raw_data = readRDS("data/river_queries/raw_data.RDS")
 
-set.seed(2839)
-test_data = sample_n(raw_data, 1000) %>%
+set.seed(2832)
+regression_data = sample_n(raw_data, 10000)
+
+regression_data = regression_data %>%
   select(text, clean_tweet, WBID) %>%
   mutate(
     senti_nrc = sentiment(
       # NRC sentiment from Mohammad & Turney (2010)
-      test_data$clean_tweet,
+      regression_data$clean_tweet,
       polarity_dt = lexicon::hash_sentiment_nrc,
       hyphen = " "
     ),
@@ -18,7 +20,7 @@ test_data = sample_n(raw_data, 1000) %>%
   mutate(
     senti_sentiword = sentiment(
       #  Baccianella, Esuli and Sebastiani's (2010)
-      test_data$clean_tweet,
+      regression_data$clean_tweet,
       polarity_dt = lexicon::hash_sentiment_sentiword,
       hyphen = " "
     ),
@@ -27,7 +29,7 @@ test_data = sample_n(raw_data, 1000) %>%
   mutate(
     senti_jockers_rinkers = sentiment(
       # Jockers (2017) & Rinker's augmented Hu & Liu (2004)
-      test_data$clean_tweet,
+      regression_data$clean_tweet,
       polarity_dt = lexicon::hash_sentiment_jockers_rinker,
       hyphen = " "
     ),
@@ -36,7 +38,7 @@ test_data = sample_n(raw_data, 1000) %>%
   mutate(
     senti_cnet = sentiment(
       # Cambria, Poria, Bajpai,& Schuller's (2016)
-      test_data$clean_tweet,
+      regression_data$clean_tweet,
       polarity_dt = lexicon::hash_sentiment_senticnet,
       hyphen = " "
     ),
@@ -45,26 +47,23 @@ test_data = sample_n(raw_data, 1000) %>%
   mutate(
     senti_google = sentiment(
       # Taboada, Brooke, Tofiloski, Voll, & Stede's (2011)
-      test_data$clean_tweet,
+      regression_data$clean_tweet,
       polarity_dt = lexicon::hash_sentiment_socal_google,
       hyphen = " "
     ),
     senti_google = senti_google$sentiment
   )
 
-for (i in 1:nrow(test_data)) {
-  test_datalog_senti[i] = log(
-    mean(
-      test_data$senti_nrc[i] + test_data$senti_sentiword[i] + test_data$senti_jockers_rinkers[i] + test_data$senti_cnet[i] + test_data$senti_google[i]
-    )
-  )
-
+scaleit = function(x) {
+  (x - min(x)) / (max(x) - min(x))
 }
 
-WB = as.data.frame(aggregate(senti_nrc ~ WBID, test_data, mean))
 
+for (i in 1:nrow(regression_data)) {
+  regression_data$log_senti[i] = mean(regression_data$senti_nrc[i] + regression_data$senti_sentiword[i] + regression_data$senti_jockers_rinkers[i] + regression_data$senti_cnet[i] + regression_data$senti_google[i])
+}
 
-
+WB = as.data.frame(aggregate(scale_senti ~ WBID, regression_data, mean))
 
 waterbody = read_sf("data/web/wb_cat_class.geojson") %>%
   st_drop_geometry() %>%
@@ -74,8 +73,10 @@ waterbody = read_sf("data/web/wb_cat_class.geojson") %>%
   na.omit(status) %>%
   mutate(status = factor(status, levels = c('Bad', 'Poor', 'Moderate', 'Good', 'NA')))
 
+reg_test = inner_join(WB,waterbody)
 
-ggplot2::ggplot(waterbody, aes(x = status, y = senticent_polarity, group = status)) +
+
+ggplot2::ggplot(reg_test, aes(x = status, y = scale_senti, group = status)) +
   ggplot2::geom_boxplot(aes(fill = status)) +
   theme_tinyhand()
 
